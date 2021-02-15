838af6d320f5797056e60347da2b42eaee3aef6b
yachen <io260188@gmail.com>
2021-02-16 01:12:33 +0800

    feat: render stream list

--- client/src/components/streams/List/index.jsx
+++ client/src/components/streams/List/index.jsx
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { fetchStreams } from '../../../actions/streams';
 
-const List = () => {
-  return <div>List</div>;
+import './index.scss';
+
+const List = ({ streams, fetchStreams }) => {
+  useEffect(() => {
+    fetchStreams();
+  }, []);
+
+  return (
+    <div className="streams-list">
+      <h1>Streams</h1>
+      {streams.map((stream) => {
+        return (
+          <div className="stream-item" key={stream.id}>
+            <img src="https://img.icons8.com/metro/26/000000/camera.png" />
+            <div className="stream-item-content">
+              <h2 className="title">{stream.title}</h2>
+              <h4 className="description">{stream.description}</h4>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
-export default List;
+const mapStateToProps = (state) => {
+  return { streams: Object.values(state.streams) };
+};
+
+export default connect(mapStateToProps, {
+  fetchStreams,
+})(List);
